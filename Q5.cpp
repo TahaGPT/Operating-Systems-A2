448a9d62fe7d545a814ce9a515531f9766f2b6ad
TahaGPT <tahaaahmed12115@gmail.com>
2025-03-15 05:53:12 +0500

    Processes Added

--- Q5.cpp
+++ Q5.cpp
@@ -13,9 +13,14 @@ int main()
     int n;
     cin >> n;
     Process *p = new Process[n];
+    int seconds = INT_MAX;
     for (int i = 0; i < n; i++)
     {
         cin >> p[i].id >> p[i].arrival >> p[i].burst;
+        if(seconds > p[i].arrival)
+            seconds = p[i].arrival;
     }
+    
+
 }
 
