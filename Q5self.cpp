876e25842a6230bace885c47a958a97c66cbe67e
TahaGPT <tahaaahmed12115@gmail.com>
2025-03-23 13:59:14 +0500

    Updated completed Code

--- Q5self.cpp
+++ Q5self.cpp
@@ -1,4 +1,125 @@
-//
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// struct Process
+// {
+//     int id;
+//     int arrival;
+//     int burst;
+//     int TAT = 0;
+//     int WAT = 0;
+//     int completion;
+//     int remaining;
+// };
+
+// struct Running
+// {
+//     int p = -1;
+//     int qN;
+// };
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     Process *p = new Process[n];
+//     int time = INT_MAX;
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> p[i].id >> p[i].arrival >> p[i].burst;
+//         p[i].remaining = p[i].burst;
+//         if (time > p[i].arrival)
+//             time = p[i].arrival;
+//     }
+//     queue<int> rr1, rr2, fcfs, complete;
+//     Running r;
+//     int total = 0;
+//     const int q1 = 4, q2 = 8;
+//     while (complete.size() != n || !(rr1.empty() || rr2.empty() || fcfs.empty()))
+//     {
+//         cout << "Loop : \n";
+//         for (int i = total; i < n && p[i].arrival <= time; i++)
+//         {
+//             cout << "Process " << i << " entered at Time " << time << " seconds\n";
+//             rr1.push(i);
+//             total++;
+//         }
+
+//         if(!rr1.empty())
+//         {
+//             int i = rr1.front();
+//             rr1.pop();
+//             time += q1;
+//             p[i].remaining -= q1;
+//             if(p[i].remaining <= 0)
+//             {
+//                 p[i].completion = time + p[i].remaining;
+//                 cout << "Process " << i << " -> " << p[i].completion << endl;
+//                 complete.push(i);
+//             }
+//             else if(p[i].remaining <= q1)
+//             {
+//                 rr1.push(i);
+//             }
+//             else if (p[i].remaining > q1)
+//             {
+//                 rr2.push(i);
+//             }
+
+//         }
+//         else if(!rr2.empty())
+//         {
+//             int i = rr2.front();
+//             rr2.pop();
+//             if(total < n && time + q2 > p[total].arrival)
+//             {
+//                 p[i].remaining -= (p[total].arrival - time);
+//                 rr2.push(i);
+//                 time += (p[total].arrival - time);
+//             }
+//             else
+//             {
+//                 p[i].remaining -= q2;
+//                 time += q2;
+//             }
+//             if (p[i].remaining <= 0)
+//             {
+//                 p[i].completion = time + p[i].remaining;
+//                 cout << "Process " << i << " -> " << p[i].completion << endl;
+//                 complete.push(i);
+//             }
+//             else if (p[i].remaining <= q2)
+//             {
+//                 rr2.push(i);
+//             }
+//             else if (p[i].remaining > q2)
+//             {
+//                 fcfs.push(i);
+//             }
+
+//         }
+//         else if(!fcfs.empty())
+//         {
+//             int i = fcfs.front();
+//             if(total < n)
+//             {
+//                 p[i].remaining -= p[total].arrival - time;
+//                 time += p[total].arrival - time;
+//             }
+//             else
+//             {
+//                 time += p[i].remaining;
+//                 p[i].remaining = 0;
+//                 p[i].completion = time;
+//                 cout << "Process " << i << " -> " << p[i].completion << endl;
+//                 complete.push(i);
+//             }
+//         }
+//         else{
+//             time++;
+//         }
+//     }
+// }
 
 #include <bits/stdc++.h>
 using namespace std;
